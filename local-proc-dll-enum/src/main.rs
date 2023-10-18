00d26c3a61b6cbb77614f5f9a467aa7f9e576c1d
Olivier POTTIEZ <olivier.pottiez@live.fr>
2023-10-18 10:42:03 +0200

    Adding local-proc-dll-enum

---
+++ local-proc-dll-enum/src/main.rs
@@ -0,0 +1,50 @@
+mod debug;
+mod winapi;
+
+use winapi::functions::__readgsqword;
+use winapi::structs::PEB;
+use winapi::structs::LDR_DATA_TABLE_ENTRY;
+use winapi::structs::LIST_ENTRY;
+
+fn show_loaded_dlls() -> Vec<String> {
+    unsafe {
+        let mut dlls: Vec<String> = vec!();
+
+        let peb_offset: *const u64 = __readgsqword(0x60)  as *const u64;
+        debug_info!(peb_offset);
+        let rf_peb: *const PEB = peb_offset as * const PEB;
+        let peb = *rf_peb;
+        let mut p_ldr_data_table_entry: *const LDR_DATA_TABLE_ENTRY = (*peb.Ldr).InMemoryOrderModuleList.Flink as *const LDR_DATA_TABLE_ENTRY;
+        let mut p_list_entry = &(*peb.Ldr).InMemoryOrderModuleList as *const LIST_ENTRY;
+
+        loop {
+            let buffer = std::slice::from_raw_parts(
+                (*p_ldr_data_table_entry).FullDllName.Buffer.as_ptr(),
+                (*p_ldr_data_table_entry).FullDllName.Length as usize / 2);
+            let dll_name = String::from_utf16_lossy(buffer);
+            
+            //last element of the list => shoudl stop the loop
+            if p_list_entry == (*peb.Ldr).InMemoryOrderModuleList.Blink {
+                return dlls
+            }
+
+            dlls.push(dll_name);
+
+            //go to next element of the list
+            p_list_entry = (*p_list_entry).Flink;
+            p_ldr_data_table_entry = (*p_list_entry).Flink as *const LDR_DATA_TABLE_ENTRY;
+        }
+    }
+}
+
+fn main() {
+    
+    
+    let dlls = show_loaded_dlls();
+
+    debug_info_msg!("List of loaded dlls in current Process : ");
+    for dll_name in dlls {
+        debug_success!("Found dll", dll_name.to_lowercase().as_str());
+    }
+    
+}
---
+++ local-proc-dll-enum/src/winapi/structs.rs
@@ -0,0 +1,97 @@
+use super::types::PWSTR;
+
+#[repr(C)]
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+pub struct PEB {
+    pub Reserved1: [u8; 2],
+    pub BeingDebugged: u8,
+    pub Reserved2: [u8; 1],
+    pub Reserved3: [*mut ::core::ffi::c_void; 2],
+    pub Ldr: *mut PEB_LDR_DATA,
+    pub ProcessParameters: *mut RTL_USER_PROCESS_PARAMETERS,
+    pub Reserved4: [*mut ::core::ffi::c_void; 3],
+    pub AtlThunkSListPtr: *mut ::core::ffi::c_void,
+    pub Reserved5: *mut ::core::ffi::c_void,
+    pub Reserved6: u32,
+    pub Reserved7: *mut ::core::ffi::c_void,
+    pub Reserved8: u32,
+    pub AtlThunkSListPtr32: u32,
+    pub Reserved9: [*mut ::core::ffi::c_void; 45],
+    pub Reserved10: [u8; 96],
+    pub PostProcessInitRoutine: PPS_POST_PROCESS_INIT_ROUTINE,
+    pub Reserved11: [u8; 128],
+    pub Reserved12: [*mut ::core::ffi::c_void; 1],
+    pub SessionId: u32,
+}
+
+impl ::core::marker::Copy for PEB {}
+impl ::core::clone::Clone for PEB {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+#[repr(C)]
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+pub struct LIST_ENTRY {
+    pub Flink: *mut LIST_ENTRY,
+    pub Blink: *mut LIST_ENTRY,
+}
+
+#[repr(C)]
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+pub struct PEB_LDR_DATA {
+    pub Reserved1: [u8; 8],
+    pub Reserved2: [*mut ::core::ffi::c_void; 3],
+    pub InMemoryOrderModuleList: LIST_ENTRY,
+}
+
+#[repr(C)]
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+pub struct RTL_USER_PROCESS_PARAMETERS {
+    pub Reserved1: [u8; 16],
+    pub Reserved2: [*mut ::core::ffi::c_void; 10],
+    pub ImagePathName: UNICODE_STRING,
+    pub CommandLine: UNICODE_STRING,
+}
+
+#[repr(C)]
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+pub struct UNICODE_STRING {
+    pub Length: u16,
+    pub MaximumLength: u16,
+    pub Buffer: PWSTR,
+}
+
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+pub type PPS_POST_PROCESS_INIT_ROUTINE = ::core::option::Option<unsafe extern "system" fn() -> ()>;
+
+#[repr(C)]
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+pub struct LDR_DATA_TABLE_ENTRY {
+    pub Reserved1: [*mut ::core::ffi::c_void; 2],
+    pub InMemoryOrderLinks: LIST_ENTRY,
+    pub Reserved2: [*mut ::core::ffi::c_void; 2],
+    pub DllBase: *mut ::core::ffi::c_void,
+    pub Reserved3: [*mut ::core::ffi::c_void; 2],
+    pub FullDllName: UNICODE_STRING,
+    pub Reserved4: [u8; 8],
+    pub Reserved5: [*mut ::core::ffi::c_void; 3],
+    pub Anonymous: LDR_DATA_TABLE_ENTRY_0,
+    pub TimeDateStamp: u32,
+}
+
+#[repr(C)]
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+pub union LDR_DATA_TABLE_ENTRY_0 {
+    pub CheckSum: u32,
+    pub Reserved6: *mut ::core::ffi::c_void,
+}
