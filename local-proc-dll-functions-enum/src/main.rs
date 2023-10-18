4f658f4146a55d0b876f8b3967939ad25c90003a
Olivier POTTIEZ <olivier.pottiez@live.fr>
2023-10-18 17:38:43 +0200

    Adding several examples

--- local-proc-dll-functions-enum/src/main.rs
+++ local-proc-dll-functions-enum/src/main.rs
@@ -212,6 +212,11 @@ fn get_proc_address(module_handle: HINSTANCE, function_name: &str) -> u64 {
     }
 }
 
+
+pub type HWND = isize;
+pub type MessageBoxA = unsafe extern "system" fn (HWND, winapi::types::PCSTR, winapi::types::PCSTR, u32) -> i32;
+pub type LoadLibraryA = unsafe extern "system" fn (winapi::types::PCSTR) -> i32;
+
 fn main() {
     //Exemple 1 : List all dlls and all functions of the current process
     // let dlls = get_loaded_dlls();
@@ -252,10 +257,31 @@ fn main() {
     // }
 
     //Exemple 3 : Find the address of a the NtMapViewOfSection
-    let dll_name = "ntdll.dll";
-    let function_name = "NtMapViewOfSection";
-    let dll_base_address = get_dll_base_address(dll_name);
-    let function_address = get_proc_address(dll_base_address, function_name);
-    debug_success_msg!(format!("Found function {} on dll {} at address {:?}", function_name, dll_name, function_address as *const u64));
+    // let dll_name = "ntdll.dll";
+    // let function_name = "NtMapViewOfSection";
+    // let dll_base_address = get_dll_base_address(dll_name);
+    // let function_address = get_proc_address(dll_base_address, function_name);
+    // debug_success_msg!(format!("Found function {} on dll {} at address {:?}", function_name, dll_name, function_address as *const u64));
+
+    //Exemple 4 loading and displaying MessageBox
+    unsafe {
+        let kernel32_base_address: HINSTANCE = get_dll_base_address("kernel32.dll");
+        debug_success_msg!(format!("kernel32.dll found at address {:?}", kernel32_base_address as *const u64));
+
+        let fn_load_library: LoadLibraryA = std::mem::transmute(get_proc_address(kernel32_base_address, "LoadLibraryA"));
+        debug_success_msg!(format!("kernel32.dll.LoadLibraryA found at address {:?}", fn_load_library as *const u64));
+
+        fn_load_library(winapi::types::PCSTR::from_raw("user32.dll\0".as_ptr()));
+        debug_success_msg!("user32.dll loaded!");
+
+        let user32_base_address: HINSTANCE = get_dll_base_address("user32.dll");
+        debug_success_msg!(format!("user32.dll found at address {:?}", user32_base_address as *const u64));
+
+        let fn_message_box: MessageBoxA = std::mem::transmute(get_proc_address(user32_base_address, "MessageBoxA"));
+        debug_success_msg!(format!("user32.dll.MessageBoxA found at address {:?}", fn_load_library as *const u64));
+
+        fn_message_box(0, winapi::types::PCSTR::from_raw("This Message box was loaded manually !\0".as_ptr()), winapi::types::PCSTR::from_raw("Test Message Box\0".as_ptr()), 0);
+        debug_success_msg!("MessageBox displayed!");
+    }
 
 }
--- local-proc-dll-functions-enum/src/winapi/types.rs
+++ local-proc-dll-functions-enum/src/winapi/types.rs
@@ -8,7 +8,6 @@ use std::{os::raw::c_ulong, ffi::c_ulonglong};
 //DWORD64 -> u64
 //ULONGLONG -> u64
 
-
 pub type DWORD = c_ulong;
 pub type __uint64 = u64;
 pub type DWORD64 = __uint64;
@@ -17,8 +16,8 @@ pub type HINSTANCE = isize;
 pub type UINT_PTR = __uint64;
 
 extern "C" {
-    // #[doc(hidden)]
-    // pub fn strlen(s: PCSTR) -> usize;
+    #[doc(hidden)]
+    pub fn strlen(s: PCSTR) -> usize;
     #[doc(hidden)]
     pub fn wcslen(s: PCWSTR) -> usize;
 }
@@ -151,3 +150,56 @@ impl PCWSTR {
     // }
 }
 
+#[repr(transparent)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct PCSTR(pub *const u8);
+
+impl PCSTR {
+    /// Construct a new `PCSTR` from a raw pointer
+    pub const fn from_raw(ptr: *const u8) -> Self {
+        Self(ptr)
+    }
+
+    /// Construct a null `PCSTR`
+    pub fn null() -> Self {
+        Self(std::ptr::null())
+    }
+
+    /// Returns a raw pointer to the `PCSTR`
+    pub fn as_ptr(&self) -> *const u8 {
+        self.0
+    }
+
+    /// Checks whether the `PCSTR` is null
+    pub fn is_null(&self) -> bool {
+        self.0.is_null()
+    }
+
+    /// String data without the trailing 0
+    ///
+    /// # Safety
+    ///
+    /// The `PCSTR`'s pointer needs to be valid for reads up until and including the next `\0`.
+    pub unsafe fn as_bytes(&self) -> &[u8] {
+        let len = strlen(*self);
+        std::slice::from_raw_parts(self.0, len)
+    }
+
+    /// Copy the `PCSTR` into a Rust `String`.
+    ///
+    /// # Safety
+    ///
+    /// See the safety information for `PCSTR::as_bytes`.
+    pub unsafe fn to_string(&self) -> std::result::Result<String, std::string::FromUtf8Error> {
+        String::from_utf8(self.as_bytes().into())
+    }
+
+    // /// Allow this string to be displayed.
+    // ///
+    // /// # Safety
+    // ///
+    // /// See the safety information for `PCSTR::as_bytes`.
+    // pub unsafe fn display(&self) -> impl std::fmt::Display + '_ {
+    //     Decode(move || decode_utf8(self.as_bytes()))
+    // }
+}
